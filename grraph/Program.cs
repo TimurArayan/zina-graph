4d00e738ff523cf8f0aa4e352e92eaea5f2592a5
timur <arayan21092004@gmail.com>
2022-11-12 08:35:16 +0300

    Добавьте файлы проекта.

---
+++ grraph/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grraph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task4");
+            clsGraph graph = new clsGraph(7);
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(0, 3);
+            graph.AddEdge(1, 0);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(1, 6);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 1);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 0);
+            graph.AddEdge(3, 4);
+            graph.AddEdge(3, 2);
+            graph.AddEdge(4, 3);
+            graph.AddEdge(4, 5);
+            graph.AddEdge(5, 2);
+            graph.AddEdge(5, 4);
+            graph.AddEdge(6, 1);
+            graph.PrintAdjacecnyMatrix();
+
+            Console.WriteLine("BFS стартует с 0:");
+            graph.BFS(0);
+            Console.WriteLine("DFS стартует с 0:");
+            graph.DFS(0);
+
+
+            Console.ReadKey();
+
+        }
+        class clsGraph
+        {
+            private int Vertices; // количество узлов
+            private List<Int32>[] adj; // смежность узлов
+
+            public clsGraph(int v) // вывод матрицы смежности
+            {
+                Vertices = v;
+                adj = new List<Int32>[v];
+                for (int i = 0; i < v; i++)
+                {
+                    adj[i] = new List<Int32>();
+                }
+
+            }
+
+
+            public void AddEdge(int v, int w) // добавляем грань от узла до узла
+            {
+                adj[v].Add(w);
+            }
+
+
+            public void BFS(int s)
+            {
+                bool[] visited = new bool[Vertices]; // флаг посещения
+
+
+                Queue<int> queue = new Queue<int>(); //
+                visited[s] = true;
+                queue.Enqueue(s);
+
+
+                while (queue.Count != 0)
+                {
+
+                    s = queue.Dequeue();
+                    Console.WriteLine("next->" + s);
+
+
+                    foreach (Int32 next in adj[s])
+                    {
+                        if (!visited[next])
+                        {
+                            visited[next] = true;
+                            queue.Enqueue(next);
+                        }
+                    }
+
+                }
+            }
+
+
+            public void DFS(int s)
+            {
+                bool[] visited = new bool[Vertices]; //проверка на посещение узла
+
+
+                Stack<int> stack = new Stack<int>();
+                visited[s] = true;
+                stack.Push(s);
+
+                while (stack.Count != 0)
+                {
+                    s = stack.Pop();
+                    Console.WriteLine("next->" + s);
+                    foreach (int i in adj[s])
+                    {
+                        if (!visited[i])
+                        {
+                            visited[i] = true;
+                            stack.Push(i);
+                        }
+                    }
+                }
+            }
+
+            public void PrintAdjacecnyMatrix()
+            {
+                for (int i = 0; i < Vertices; i++)
+                {
+                    Console.Write(i + ":[");
+                    string s = "";
+                    foreach (var k in adj[i])
+                    {
+                        s = s + (k + ",");
+                    }
+                    s = s.Substring(0, s.Length - 1);
+                    s = s + "]";
+                    Console.Write(s);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ zina+graph/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zina_graph
+{
+    public struct Person
+    {
+        public string name;
+        public int schet;
+        public string problem;
+        public byte temperament;
+        public byte mind;
+
+        public Person(string name, int schet, string problem, byte temperament, byte mind)
+        {
+            this.name = name;
+            this.schet = schet;
+            this.problem = problem;
+            this.temperament = temperament;
+            this.mind = mind;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 3. Очередь в ЖЭК");
+            Console.WriteLine();
+            Random random = new Random();
+            List<Person> turn1 = new List<Person>();
+            List<Person> turn2 = new List<Person>();
+            List<Person> turn3 = new List<Person>();
+            Stack<Person> Sina = new Stack<Person>();
+            List<Person> Sina_temp = new List<Person>
+        {
+            new Person("Николай", 9014, "подключение отопления", 8, 1),
+            new Person("Валентина", 8479, "оплата отопления", 3, 0),
+            new Person("Петр", 2981, "слишком высокие цены", 9, 1),
+            new Person("Константин", 5695, "оплата отопления", 5, 0),
+            new Person("Наташа", 2958, "подключение отопления", 3, 1),
+            new Person("Владислав", 9124, "подключение отопления", 6, 1)
+        };
+            for (int i = Sina_temp.Count - 1; i != -1; i--)
+            {
+                Sina.Push(Sina_temp[i]);
+            }
+            foreach (Person x in Sina)
+            {
+                if (x.problem == "подключение отопления")
+                {
+                    if (x.mind == 0)
+                    {
+                        int num1 = random.Next(2, 3);
+                        if (num1 == 2)
+                        {
+                            turn2.Add(x);
+                        }
+                        else
+                        {
+                            turn3.Add(x);
+                        }
+                    }
+                    else
+                    {
+                        turn1.Add(x);
+                    }
+                }
+                else if (x.problem == "оплата отопления")
+                {
+                    if (x.mind == 0)
+                    {
+                        int num2 = random.Next(1, 3);
+                        if (num2 == 1)
+                        {
+                            turn1.Add(x);
+                        }
+                        else if (num2 == 2)
+                        {
+                            turn1.Add(x);
+                        }
+                        else
+                        {
+                            turn3.Add(x);
+                        }
+                    }
+                    else
+                    {
+                        turn2.Add(x);
+                    }
+                }
+                else
+                {
+                    if (x.mind == 0)
+                    {
+                        int num3 = random.Next(1, 2);
+                        if (num3 == 1)
+                        {
+                            turn1.Add(x);
+                        }
+                        else
+                        {
+                            turn2.Add(x);
+                        }
+                    }
+                    else
+                    {
+                        turn3.Add(x);
+                    }
+                }
+            }
+            for (int i = 1; i < turn1.Count; i++)
+            {
+                if (turn1[i].temperament >= 5)
+                {
+                    var temp = turn1[i];
+                    Console.WriteLine($"{turn1[i].name} скандалист, введите сколько людей он/она обгонит (можно обогнать {i} людей)");
+                    byte num = byte.Parse(Console.ReadLine());
+                    while (temp.name != turn1[i - num].name)
+                    {
+                        for (int j = i; j > i - num; j--)
+                        {
+                            (turn1[j], turn1[j - 1]) = (turn1[j - 1], turn1[j]);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 1; i < turn2.Count; i++)
+            {
+                if (turn2[i].temperament >= 5)
+                {
+                    var temp = turn2[i];
+                    Console.WriteLine($"{turn2[i].name} скандалист, введите сколько людей он/она обгонит (можно обогнать {i} людей)");
+                    byte num = byte.Parse(Console.ReadLine());
+                    while (temp.name != turn2[i - num].name)
+                    {
+                        for (int j = i; j > i - num; j--)
+                        {
+                            (turn2[j], turn2[j - 1]) = (turn2[j - 1], turn2[j]);
+                        }
+                    }
+                }
+            }
+
+            for (int i = 1; i < turn3.Count; i++)
+            {
+                if (turn3[i].temperament >= 5)
+                {
+                    var temp = turn3[i];
+                    Console.WriteLine($"{turn3[i].name} скандалист, введите сколько людей он/она обгонит (можно обогнать {i} людей)");
+                    byte num = byte.Parse(Console.ReadLine());
+                    while (temp.name != turn3[i - num].name)
+                    {
+                        for (int j = i; j > i - num; j--)
+                        {
+                            (turn3[j], turn3[j - 1]) = (turn3[j - 1], turn3[j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
